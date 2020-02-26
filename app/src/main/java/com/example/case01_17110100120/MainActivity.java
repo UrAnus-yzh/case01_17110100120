8d9205a768cf9deb276edef665571c7023182dd8
yanzihao <2621404471@qq.com>
2020-02-26 19:12:36 +0800

    5

--- app/src/main/java/com/example/case01_17110100120/MainActivity.java
+++ app/src/main/java/com/example/case01_17110100120/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    private Button btnxianshi,btnyinshi,btncalla;
+    private Button btnxianshi,btnyinshi,btncallaa;
 
 
 }
